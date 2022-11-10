737e2b2369cbd9e88663cc4a75d31ab76a006427
Uwaoma Cyruz <uwaomacyruz@gmail.com>
2022-11-09 23:20:06 -0800

    added an error icon

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -3,8 +3,16 @@ import { Link } from "react-router-dom";
 const NoMatch = () => {
   return (
     <div>
-      <h1 className="error_msg">Error 404</h1>
-      <h2 className="error_msg">Page Not Found</h2>
+      <h2 className="error_msg">
+        <span className="EP">o</span>oops...
+      </h2>
+      <img
+        src="https://media.gettyimages.com/id/468164033/vector/error-404.jpg?s=2048x2048&w=gi&k=20&c=RdSTMxRpODdRsEtDcTfAHr7G1ThGS7slpnl40uvJddY="
+        alt="Error Page"
+        width="300px"
+        height="300px"
+        className="EI"
+      />
       <Link className="nested_route" to="/dashboard">
         dashboard
       </Link>
